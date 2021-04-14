8c6b18b7b654784b3949917cadf26fea9b8a512f
Daniel Perna <danielperna84@gmail.com>
2021-04-14 22:09:13 +0200

    input_select improvements

--- hahomematic/platforms/input_select.py
+++ hahomematic/platforms/input_select.py
@@ -25,18 +25,22 @@ class input_select(Entity):
     def STATE(self):
         try:
             if self._state is None and self.operations & OPERATION_READ:
-                self._state = self.value_list[self.proxy.getValue(self.address, self.parameter)]
+                self._state = self.proxy.getValue(self.address, self.parameter)
         # pylint: disable=broad-except
         except Exception as err:
             LOG.info("input_select: Failed to get state for %s, %s, %s: %s",
                      self.device_type, self.address, self.parameter, err)
             return None
-        return self._state
+        return self.value_list[self._state]
 
     @STATE.setter
     def STATE(self, value):
         try:
-            self.proxy.setValue(self.address, self.parameter, self.value_list.index(value))
+            # We allow setting the value via index as well, just in case.
+            if isinstance(value, int):
+                self.proxy.setValue(self.address, self.parameter, value)
+            else:
+                self.proxy.setValue(self.address, self.parameter, self.value_list.index(value))
         # pylint: disable=broad-except
         except Exception:
             LOG.exception("input_select: Failed to set state for: %s, %s, %s, %s",
