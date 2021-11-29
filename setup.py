21aa80a1eaeb8a9af5aa37d248758fecc35d27ad
Markus Jankowski <markus@mm-jankowski.de>
2021-11-29 16:58:51 +0100

    update changelog and version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 PACKAGE_NAME = "hahomematic"
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = "0.0.12"
+VERSION = "0.0.13"
 
 PACKAGES = find_packages(exclude=["tests", "tests.*", "dist", "build"])
 
