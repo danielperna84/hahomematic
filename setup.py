62479fb25297a159ca373ce31cb8fffdd6f7d2c3
Markus Jankowski <markus@mm-jankowski.de>
2021-12-12 09:02:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ def readme():
     with open("README.md") as fptr:
         return fptr.read()
 
+
 package_data = {
         'foopkg': ['py.typed'],
     },
 PACKAGE_NAME = "hahomematic"
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = "0.0.18"
+VERSION = "0.0.19"
 
 PACKAGES = find_packages(exclude=["tests", "tests.*", "dist", "build"])
 
@@ -30,7 +31,7 @@ setup(
     author_email="danielperna84@gmail.com",
     description="Homematic interface for Home Assistant",
     packages=PACKAGES,
-    package_data={ 'hahomematic': ['py.typed'],},
+    package_data={'hahomematic': ['py.typed']},
     zip_safe=False,
     platforms="any",
     python_requires=">=3.8",
