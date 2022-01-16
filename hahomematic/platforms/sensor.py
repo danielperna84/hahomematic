63a573bf053f23326a84b197d61f2722ce8b82af
SukramJ <sukramj@icloud.com>
2022-01-16 13:32:39 +0100

    Add converter to sensor value (#194)
    * Add converter to sensor value
    
    * raise connection_checker interval
    
    Add sleep interval to wait with reconnect after successful connection check
    
    * Update version

--- hahomematic/platforms/sensor.py
+++ hahomematic/platforms/sensor.py
@@ -42,4 +42,47 @@ class HmSensor(GenericEntity[Any]):
         """Return the value."""
         if self._value is not None and self._value_list is not None:
             return self._value_list[self._value]
+        if convert_func := self._get_converter_func():
+            return convert_func(self._value)
         return self._value
+
+    def _get_converter_func(self) -> Any:
+        """Return a converter based on sensor."""
+        if convert_func := CONVERTERS_BY_DEVICE_PARAM.get(
+            (self.device_type, self.parameter)
+        ):
+            return convert_func
+        if convert_func := CONVERTERS_BY_PARAM.get(self.parameter):
+            return convert_func
+
+
+def _convert_float_to_int(value: Any) -> int | None:
+    """Convert value to int."""
+    if value is not None and isinstance(value, float):
+        return int(value)
+    return value
+
+
+def _fix_rssi(value: Any) -> int | None:
+    """Fix rssi value."""
+    if value is None or not isinstance(value, int):
+        return None
+    if -120 <= value <= 0:
+        return value
+    if 0 < value <= 120:
+        return value * -1
+    if -256 <= value <= -146:
+        return (value * -1) - 256
+    if 146 <= value <= 256:
+        return value - 256
+    return None
+
+
+CONVERTERS_BY_DEVICE_PARAM: dict[tuple[str, str], Any] = {
+    ("HmIP-SCTH230", "CONCENTRATION"): _convert_float_to_int,
+}
+
+CONVERTERS_BY_PARAM: dict[str, Any] = {
+    "RSSI_PEER": _fix_rssi,
+    "RSSI_DEVICE": _fix_rssi,
+}
