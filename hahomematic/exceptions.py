cc754374ce758810832e5c0053bc56c69853b0af
SukramJ <sukramj@icloud.com>
2022-02-22 19:22:13 +0100

    Remove password from Exceptions (#307)

--- hahomematic/exceptions.py
+++ hahomematic/exceptions.py
@@ -1,7 +1,12 @@
 """Module for HaHomematicExceptions."""
 from __future__ import annotations
 
+import re
 from typing import Any
+
+USERNAME_PASSWORD_DIVIDER = ":"
+CREDENTIALS_URL_DIVIDER = "@"
+REPLACE_TEXT = f"{USERNAME_PASSWORD_DIVIDER}<PASSWORD>{CREDENTIALS_URL_DIVIDER}"
 
 
 class BaseHomematicException(Exception):
@@ -9,7 +14,7 @@ class BaseHomematicException(Exception):
 
     def __init__(self, name: str, *args: Any) -> None:
         """Init the HaHomematicException."""
-        super().__init__(*args)
+        super().__init__(_replace_password_in_args(args))
         self.name = name
 
 
@@ -35,3 +40,52 @@ class HaHomematicException(BaseHomematicException):
     def __init__(self, *args: Any) -> None:
         """Init the HaHomematicException."""
         super().__init__("HaHomematicException", *args)
+
+
+def _replace_password_in_args(args: Any) -> Any:
+    """Replace in args."""
+    if isinstance(args, str):
+        return _replace_password_in_text(input_text=args)
+    if isinstance(args, Exception):
+        return _replace_password_in_exception(parent_exception=args)
+    if isinstance(args, dict):
+        return _replace_password_in_dict(input_dict=args)
+    if isinstance(args, tuple):
+        new_args: list[Any] = []
+        for arg in args:
+            if isinstance(arg, str):
+                new_args.append(_replace_password_in_text(input_text=arg))
+            elif isinstance(arg, Exception):
+                new_args.append(_replace_password_in_exception(parent_exception=arg))
+            else:
+                new_args.append(arg)
+        return tuple(new_args)
+    return args
+
+
+def _replace_password_in_exception(parent_exception: Exception) -> Exception:
+    """Try replace password by special string."""
+    parent_exception.__dict__.update(
+        _replace_password_in_dict(parent_exception.__dict__)
+    )
+    return parent_exception
+
+
+def _replace_password_in_dict(input_dict: dict[str, Any]) -> dict[str, Any]:
+    """Try replace password in dict by special string."""
+
+    for k, value in input_dict.items():
+        if isinstance(value, str):
+            input_dict[k] = _replace_password_in_text(input_text=value)
+        if isinstance(value, dict):
+            input_dict[k] = _replace_password_in_dict(input_dict=value)
+    return input_dict
+
+
+def _replace_password_in_text(input_text: str) -> str:
+    """Try replace password by special string."""
+
+    regex = f"{USERNAME_PASSWORD_DIVIDER}(.*){CREDENTIALS_URL_DIVIDER}"
+    if replaced_text := re.sub(regex, REPLACE_TEXT, input_text):
+        return replaced_text
+    return input_text
