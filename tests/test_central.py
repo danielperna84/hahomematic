75769af57765b50e0f215d7b94fc62a602e23285
SukramJ <sukramj@icloud.com>
2022-06-06 12:03:44 +0200

    Fix test

--- tests/test_central.py
+++ tests/test_central.py
@@ -94,8 +94,8 @@ async def test_central(central, loop) -> None:
 
     assert usage_types[HmEntityUsage.ENTITY_NO_CREATE] == 2094
     assert usage_types[HmEntityUsage.CE_PRIMARY] == 167
-    assert usage_types[HmEntityUsage.ENTITY] == 2566
-    assert usage_types[HmEntityUsage.CE_VISIBLE] == 63
+    assert usage_types[HmEntityUsage.ENTITY] == 2540
+    assert usage_types[HmEntityUsage.CE_VISIBLE] == 89
     assert usage_types[HmEntityUsage.CE_SECONDARY] == 126
 
     assert len(central.hm_devices) == 362
