ecf2f349232c47a0f25cc928058c774d13f38e40
SukramJ <sukramj@icloud.com>
2022-08-22 20:48:58 +0200

    Remove platform from device availability event (#598)

--- hahomematic/entity.py
+++ hahomematic/entity.py
@@ -568,7 +568,6 @@ class GenericEntity(BaseParameterEntity[ParameterT], CallbackEntity):
             ATTR_ADDRESS: self.device.device_address,
             ATTR_DEVICE_TYPE: self.device.device_type,
             ATTR_PARAMETER: self.parameter,
-            ATTR_PLATFORM: self.platform.value,
             ATTR_VALUE: value,
         }
 
