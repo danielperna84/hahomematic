b99c174570ae86feb46bcc1972c0b507eb4485ea
SukramJ <sukramj@icloud.com>
2023-01-03 10:50:25 +0100

    Use local client in tests (#802)
    * Use local client in tests
    
    * Update test_central.py

---
+++ tests/helper.py
@@ -0,0 +1,43 @@
+"""Helpers for tests."""
+from __future__ import annotations
+
+import logging
+from unittest.mock import AsyncMock, MagicMock, Mock, patch
+
+import const
+
+from hahomematic.central_unit import CentralConfig, CentralUnit
+from hahomematic.client import InterfaceConfig, LocalRessources
+
+
+class CentralUnitLocalFactory:
+    """Factory for central units."""
+
+    async def get_central(
+        self, address_device_translation: dict[str, str]
+    ) -> CentralUnit:
+        """Returns a central based on give address_device_translation."""
+        interface_configs = {
+            InterfaceConfig(
+                central_name=const.CENTRAL_NAME,
+                interface="Local",
+                port=2002,
+                local_resources=LocalRessources(
+                    address_device_translation=address_device_translation
+                ),
+            )
+        }
+
+        central_unit = await CentralConfig(
+            name=const.CENTRAL_NAME,
+            host=const.CCU_HOST,
+            username=const.CCU_USERNAME,
+            password=const.CCU_PASSWORD,
+            central_id="test1234",
+            storage_folder="homematicip_local",
+            interface_configs=interface_configs,
+            default_callback_port=54321,
+        ).get_central()
+        await central_unit.start()
+
+        return central_unit
--- tests/test_central.py
+++ tests/test_central.py
@@ -6,170 +6,28 @@ from conftest import (
     get_hm_device,
     get_hm_generic_entity,
     get_value_from_generic_entity,
-    send_device_value_to_ccu,
 )
+import const
+from const import LOCAL_INTERFACE_ID
+import helper
 import pytest
-import json
-import os
 
-from hahomematic.entity import GenericEntity
-from hahomematic.const import HmEntityUsage, DEFAULT_ENCODING
 from hahomematic.custom_platforms.climate import CeRfThermostat
-from hahomematic.decorators import (
-    get_public_attributes_for_value_property,
-    get_public_attributes_for_config_property,
-)
 
 
 @pytest.mark.asyncio
-async def test_central(central, loop) -> None:
-    """Test the central."""
-    assert central
-    assert central.name == "ccu-dev"
-    assert central.model == "PyDevCCU"
-    assert central.get_client("Test-BidCos-RF").model == "PyDevCCU"
-    assert central.get_primary_client().model == "PyDevCCU"
-
-    data = {}
-    for device in central.devices.values():
-        if device.device_type not in data:
-            data[device.device_type] = {}
-        for entity in device.generic_entities.values():
-            if entity.parameter not in data[device.device_type]:
-                data[device.device_type][entity.parameter] = f"{entity.hmtype}"
-        pub_value_props = get_public_attributes_for_value_property(
-            data_object=device
-        )
-        assert pub_value_props
-        pub_config_props = get_public_attributes_for_config_property(
-            data_object=device
-        )
-        assert pub_config_props
-
-    custom_entities = []
-    for device in central.devices.values():
-        custom_entities.extend(device.custom_entities.values())
-
-    ce_channels = {}
-    for custom_entity in custom_entities:
-        if custom_entity.device.device_type not in ce_channels:
-            ce_channels[custom_entity.device.device_type] = []
-        ce_channels[custom_entity.device.device_type].append(custom_entity.channel_no)
-        pub_value_props = get_public_attributes_for_value_property(
-            data_object=custom_entity
-        )
-        assert pub_value_props
-        pub_config_props = get_public_attributes_for_config_property(
-            data_object=custom_entity
-        )
-        assert pub_config_props
-
-    entity_types = {}
-    for entity in central.entities.values():
-        if hasattr(entity, "hmtype"):
-            if entity.hmtype not in entity_types:
-                entity_types[entity.hmtype] = {}
-            if type(entity).__name__ not in entity_types[entity.hmtype]:
-                entity_types[entity.hmtype][type(entity).__name__] = []
-
-            entity_types[entity.hmtype][type(entity).__name__].append(entity)
-        pub_value_props = get_public_attributes_for_value_property(
-            data_object=entity
-        )
-        assert pub_value_props
-        pub_config_props = get_public_attributes_for_config_property(
-            data_object=entity
-        )
-        assert pub_config_props
-
-    parameters = []
-    for entity in central.entities.values():
-        if hasattr(entity, "parameter"):
-            if entity.parameter not in parameters:
-                parameters.append(entity.parameter)
-
-    units = set()
-    for entity in central.entities.values():
-        if hasattr(entity, "_unit"):
-            units.add(entity._unit)
-
-    lowbats = set()
-    for entity in central.entities.values():
-        if hasattr(entity, "parameter") and entity.parameter == "LOWBAT":
-            lowbats.add(entity.device.device_type)
-    lowbats_sorted = sorted(lowbats)
-    print(lowbats_sorted)
-
-    usage_types: dict[HmEntityUsage, int] = {}
-    for entity in central.entities.values():
-        if hasattr(entity, "usage"):
-            if entity.usage not in usage_types:
-                usage_types[entity.usage] = 0
-            counter = usage_types[entity.usage]
-            usage_types[entity.usage] = counter + 1
-
-    switches: dict[str, set[int]] = {}
-
-    for entity in central.entities.values():
-        # if isinstance(entity, HmSwitchPlatform):
-        if hasattr(entity, "parameter") and entity.parameter == "ON_TIME":
-            device_type = entity.device.device_type[:8]
-            if device_type.lower().startswith("hmip"):
-                continue
-
-            channel_no = entity.channel_no
-            if device_type not in switches:
-                switches[device_type] = set()
-            switches[device_type].add(channel_no)
-
-    entity_type_operations: dict[str, dict[str, set[int]]] = {}
-    for entity in central.entities.values():
-        if isinstance(entity, GenericEntity):
-            if entity.platform not in entity_type_operations:
-                entity_type_operations[entity.platform] = {}
-
-            if entity.hmtype not in entity_type_operations[entity.platform]:
-                entity_type_operations[entity.platform][entity.hmtype] = set()
-            entity_type_operations[entity.platform][entity.hmtype].add(
-                entity._attr_operations
-            )
-    addresses: dict[str, str] = {}
-    for address, device in central.devices.items():
-        if device.device_type.startswith("HmIP"):
-            addresses[address] = f"{device.device_type}.json"
-
-    with open(
-        file=os.path.join(central.config.storage_folder, "hmip_device.json"),
-        mode="w",
-        encoding=DEFAULT_ENCODING,
-    ) as fptr:
-        json.dump(addresses, fptr)
-
-    assert usage_types[HmEntityUsage.ENTITY_NO_CREATE] == 2398
-    assert usage_types[HmEntityUsage.CE_PRIMARY] == 168
-    assert usage_types[HmEntityUsage.ENTITY] == 3570
-    assert usage_types[HmEntityUsage.CE_VISIBLE] == 88
-    assert usage_types[HmEntityUsage.CE_SECONDARY] == 128
-
-    assert len(central.devices) == 362
-    assert len(central.entities) == 6252
-    assert len(data) == 362
-    assert len(custom_entities) == 296
-    assert len(ce_channels) == 104
-    assert len(entity_types) == 6
-    assert len(parameters) == 184
-
-
-@pytest.mark.asyncio
-async def test_device_set_data(central, pydev_ccu, loop) -> None:
+async def test_device_set_data(
+    central_local_factory: helper.CentralUnitLocalFactory,
+) -> None:
     """Test callback."""
+    assert central_local_factory
+    central = await central_local_factory.get_central({"VCU6354483": "HmIP-STHD.json"})
     assert central
-    assert pydev_ccu
     old_value = await get_value_from_generic_entity(
         central_unit=central, address="VCU6354483:1", parameter="SET_POINT_TEMPERATURE"
     )
-    assert old_value == 4.5
-    send_device_value_to_ccu(pydev_ccu, "VCU6354483:1", "SET_POINT_TEMPERATURE", 19.0)
+    assert old_value is None
+    central.event(LOCAL_INTERFACE_ID, "VCU6354483:1", "SET_POINT_TEMPERATURE", 19.0)
     new_value = await get_value_from_generic_entity(
         central, "VCU6354483:1", "SET_POINT_TEMPERATURE"
     )
@@ -177,10 +35,13 @@ async def test_device_set_data(central, pydev_ccu, loop) -> None:
 
 
 @pytest.mark.asyncio
-async def test_action_on_lock(central, pydev_ccu, loop) -> None:
+async def test_action_on_lock(
+    central_local_factory: helper.CentralUnitLocalFactory,
+) -> None:
     """Test callback."""
+    assert central_local_factory
+    central = await central_local_factory.get_central({"VCU9724704": "HmIP-DLD.json"})
     assert central
-    assert pydev_ccu
     lock = await get_hm_custom_entity(
         central_unit=central, address="VCU9724704", channel_no=1, do_load=True
     )
@@ -191,29 +52,39 @@ async def test_action_on_lock(central, pydev_ccu, loop) -> None:
 
 
 @pytest.mark.asyncio
-async def test_device_export(central, pydev_ccu, loop) -> None:
+async def test_device_export(
+    central_local_factory: helper.CentralUnitLocalFactory,
+) -> None:
     """Test device export."""
+    assert central_local_factory
+    central = await central_local_factory.get_central({"VCU6354483": "HmIP-STHD.json"})
     assert central
-    assert pydev_ccu
     hm_device = get_hm_device(central_unit=central, address="VCU6354483")
     assert hm_device
     await hm_device.export_device_definition()
 
 
 @pytest.mark.asyncio
-async def test_all_parameters(central, pydev_ccu, loop) -> None:
+async def test_all_parameters(
+    central_local_factory: helper.CentralUnitLocalFactory,
+) -> None:
     """Test device export."""
+    assert central_local_factory
+    central = await central_local_factory.get_central({"VCU6354483": "HmIP-STHD.json"})
     assert central
-    assert pydev_ccu
-    parameters = central.paramset_descriptions.get_all_parameters()
+    parameters = central.paramset_descriptions.get_all_readable_parameters()
     assert parameters
+    assert len(parameters) == 26
 
 
 @pytest.mark.asyncio
-async def test_device_hm_heatgroup(central, pydev_ccu, loop) -> None:
+async def test_device_hm_heatgroup(
+    central_local_factory: helper.CentralUnitLocalFactory,
+) -> None:
     """Test callback."""
+    assert central_local_factory
+    central = await central_local_factory.get_central({"INT0000001": "HM-CC-VG-1.json"})
     assert central
-    assert pydev_ccu
     entity = await get_hm_generic_entity(central, "INT0000001:1", "SET_TEMPERATURE")
     old_value = entity.value
     assert old_value is None
@@ -228,3 +99,14 @@ async def test_device_hm_heatgroup(central, pydev_ccu, loop) -> None:
     assert new_value == 19.0
     assert custom_entity._e_setpoint.value == 19.0
     assert custom_entity.target_temperature == 19.0
+
+    await central.put_paramset(
+        const.LOCAL_INTERFACE_ID,
+        "INT0000001:1",
+        paramset_key="VALUES",
+        value={"SET_TEMPERATURE": 12.0},
+    )
+    new_value2 = await get_value_from_generic_entity(
+        central, "INT0000001:1", "SET_TEMPERATURE"
+    )
+    assert new_value2 == 12.0
