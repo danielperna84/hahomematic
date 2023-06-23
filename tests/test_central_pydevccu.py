97ddd0e23a26acc69ac0f42582f4bb0687bbeae9
SukramJ <sukramj@icloud.com>
2023-06-23 11:52:03 +0200

    Avoid creating entities that are not usable in selected device operation mode for hmIP-RGBW

--- tests/test_central_pydevccu.py
+++ tests/test_central_pydevccu.py
@@ -112,11 +112,11 @@ async def test_central_full(central_unit_full) -> None:
     ) as fptr:
         fptr.write(orjson.dumps(addresses, option=orjson.OPT_INDENT_2 | orjson.OPT_NON_STR_KEYS))
 
-    assert usage_types[HmEntityUsage.ENTITY_NO_CREATE] == 3053
+    assert usage_types[HmEntityUsage.ENTITY_NO_CREATE] == 3056
     assert usage_types[HmEntityUsage.CE_PRIMARY] == 186
     assert usage_types[HmEntityUsage.ENTITY] == 3303
     assert usage_types[HmEntityUsage.CE_VISIBLE] == 98
-    assert usage_types[HmEntityUsage.CE_SECONDARY] == 151
+    assert usage_types[HmEntityUsage.CE_SECONDARY] == 148
 
     assert len(ce_channels) == 114
     assert len(entity_types) == 6
