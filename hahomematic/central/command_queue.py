d9db93cc8de141938bbb84fcdd29ff0f1ef1f64a
SukramJ <sukramj@icloud.com>
2024-10-03 09:49:27 +0200

    Improve command_consumer error handling

--- hahomematic/central/command_queue.py
+++ hahomematic/central/command_queue.py
@@ -2,12 +2,13 @@
 
 from __future__ import annotations
 
-from asyncio import Queue, QueueEmpty, sleep
+from asyncio import Queue, QueueEmpty
 from collections.abc import Callable
 import logging
 from typing import Final
 
 from hahomematic.async_support import Looper
+from hahomematic.exceptions import BaseHomematicException
 from hahomematic.support import reduce_args
 
 _LOGGER = logging.getLogger(__name__)
@@ -19,8 +20,7 @@ class CommandQueueHandler:
     def __init__(self) -> None:
         """Init the SendCommandQueueHandler."""
         self._queues: Final[dict[str, Queue[Callable | None]]] = {}
-        self._exit = False
-        self._looper = Looper()
+        self._looper: Final = Looper()
 
     def empty_queue(self, address: str) -> None:
         """Empty a queue for a device."""
@@ -39,12 +39,46 @@ class CommandQueueHandler:
             self._looper.create_task(_command_consumer(queue), name=f"command consumer-{address}")
             self._queues[address] = queue
         await self._queues[address].put(command)
-        await sleep(0.1)
 
     async def stop(self) -> None:
         """Add None to queue to stop the consumer."""
         for queue in self._queues.values():
             await queue.put(None)
+        await self._looper.block_till_done()
+
+
+class DeviceQueueHandler:
+    """Queue handler for sending queued commands."""
+
+    def __init__(self, address: str) -> None:
+        """Init the SendCommandQueueHandler."""
+        self._address: Final = address
+        self._queue: Queue[Callable | None] | None = None
+        self._looper: Final = Looper()
+
+    def empty_queue(self) -> None:
+        """Empty a queue for a device."""
+        if self._queue:
+            try:
+                while not self._queue.empty():
+                    self._queue.get_nowait()
+                    self._queue.task_done()
+            except QueueEmpty:
+                pass
+
+    async def put(self, command: Callable) -> None:
+        """Put command to queue."""
+        if not self._queue:
+            self._queue = Queue()
+            self._looper.create_task(
+                _command_consumer(self._queue), name=f"command consumer-{self._address}"
+            )
+        await self._queue.put(command)
+
+    async def stop(self) -> None:
+        """Add None to queue to stop the consumer."""
+        if self._queue:
+            await self._queue.put(None)
         await self._looper.block_till_done()
 
 
@@ -55,9 +89,18 @@ async def _command_consumer(queue: Queue) -> None:
         try:
             if (command := await queue.get()) is None:
                 break
-            await command()
-            queue.task_done()
+            try:
+                await command()
+            except BaseHomematicException as bhe:
+                logging.getLogger(command.func.__module__).warning(
+                    "%s with params [%s] failed: %s",
+                    command.func.__name__.upper(),
+                    f"{command.args} {"/".join([str(kw) for kw in command.keywords.values()])}",
+                    reduce_args(args=bhe.args),
+                )
+            finally:
+                queue.task_done()
         except Exception as ex:
-            _LOGGER.debug(
-                "COMMAND_CONSUMER: Unable do deque command: %s", reduce_args(args=ex.args)
+            _LOGGER.warning(
+                "COMMAND_CONSUMER: command could not be dequeued: %s", reduce_args(args=ex.args)
             )
